3df001f1593052b3f3ee40d11fc2e369011b56ec
Parth Paradkar <parthparadkar3@gmail.com>
2019-05-13 18:32:21 +0530

    Initial Commit

---
+++ models/Team.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose');
+
+const Team = new mongoose.Schema({
+	teamName: {type:String, trim:true, default:''},
+	city: {type:String, trim:true, default:''},
+})
+
+module.exports = mongoose.model('Team', Team);
